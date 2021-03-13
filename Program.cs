c83547bacac1541eafba3ac39d7ba9ed2cc1d14e
sanjana k <sanjanarthi@gmail.com>
2021-03-13 19:15:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationday10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to unit testing of user registration");
+        }
+    }
+}
