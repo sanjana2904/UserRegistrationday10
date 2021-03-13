3fdb2df39d35ca15c7e55a16f35b13023923ea81
sanjana k <sanjanarthi@gmail.com>
2021-03-13 20:04:29 +0530

    "Added UC1"

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UserRegistrationday10
 {
-    class Program
+    public class Program
     {
+        public bool validateFirstName(string firstName)
+        {
+            string pattern = @"^[A-Z]\w{2,}$";
+            bool result = Regex.IsMatch(firstName, pattern);
+            return result;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to unit testing of user registration");
+            bool result;
+
+            Program program = new Program();
+            result =  program.validateFirstName("Sanjana");
+            Console.WriteLine("First name validation result: " + result);
         }
     }
 }
